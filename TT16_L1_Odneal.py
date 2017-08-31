e7f102bd8b455ce3dde2956a71c0aa7123f7d3ba
Susie <susie.odneal@gmail.com>
2017-08-31 18:31:40 -0500

    Added TT16_L1_Odneal.py
    Uploaded first lab file

---
+++ TT16_L1_Odneal.py
@@ -0,0 +1,93 @@
+# Name: Susie Odneal
+# COSC1336, Lab 1, Fix print syntax errors
+#
+# The easiest program just prints out a few words.
+# But when learning a brand new language, even something
+# "easy" can be difficult. Find and fix any errors below.
+# There should be a space between words in your output.
+# Each statement should print one line of text.
+# Repair each print instruction so it outputs correctly.
+# If it is NOT broken, don't fix it.
+
+print ("let's get started")
+print ("does this work?")
+print ("working or not")
+print ("how about this?")
+print ("is this right?")
+print ("Can you see me now?")
+print ("how many errors can you find?")
+print ("success, I think")
+print ("What if I put a period on the end?")
+print ("What if I put a semicolon on the end?")
+print ("What could possibly be wrong")
+print ("\"this\" \"that\" \"and\" \"the\" \"other\"")
+print ("one"); print("two"); print("three");
+print ("tic" ); print("tak"); print("toe");
+print ("anybody knows that prnt means print")
+print ("I cannot wait any longer")
+print ("good grief! This is messed up, too!")
+print ("try it this way!!")
+print ("Who cares where the verb goes?")
+print ("Start End")
+print ("give me a shout when it works")
+print ("don't mess up on me now!")
+print ("Please just do what I say!")
+print ("The best is yet to come.")
+print ("In C++, this works. What about python?")
+print            ("Can I do this??")
+print   ("If this were easy, anyone could do it!")
+print ("Quote the raven: \"Nevermore\"")
+print ("Here are four quote marks: \"\"\"\"")
+print ("How could something so easy be so difficult?")
+print ("does it matter uppercase or lowercase?")
+print ('this ought to work')
+print ("One letter at a time.")
+print ("That is about enough of this.")
+print ("Good-bye, and good luck.")
+
+# Paste your output below:
+# Hint: You can "comment out" your output using:
+# Format -> Comment Out Region
+##let's get started
+##does this work?
+##working or not
+##how about this?
+##is this right?
+##Can you see me now?
+##how many errors can you find?
+##success, I think
+##What if I put a period on the end?
+##What if I put a semicolon on the end?
+##What could possibly be wrong
+##"this" "that" "and" "the" "other"
+##one
+##two
+##three
+##tic
+##tak
+##toe
+##anybody knows that prnt means print
+##I cannot wait any longer
+##good grief! This is messed up, too!
+##try it this way!!
+##Who cares where the verb goes?
+##Start End
+##give me a shout when it works
+##don't mess up on me now!
+##Please just do what I say!
+##The best is yet to come.
+##In C++, this works. What about python?
+##Can I do this??
+##If this were easy, anyone could do it!
+##Quote the raven: "Nevermore"
+##Here are four quote marks: """"
+##How could something so easy be so difficult?
+##does it matter uppercase or lowercase?
+##this ought to work
+##One letter at a time.
+##That is about enough of this.
+##Good-bye, and good luck.
+
+
+
+
