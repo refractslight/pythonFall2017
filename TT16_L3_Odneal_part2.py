f193e6d4b6f48277ec892894048e03d5a2099dc8
Susie <susie.odneal@gmail.com>
2017-09-24 09:40:50 -0500

    Finished Hacking

---
+++ TT16_L3_Odneal_part2.py
@@ -0,0 +1,78 @@
+#Name: Susie Odneal
+# COSC1336, Lab 3, part 2
+
+import time
+import sys
+import random
+
+
+def start():
+    print("|---------- WELCOME TO HAXX0R-TR0N 2k ----------|\n")
+    print("What do you want to hack?\n")
+    hack = input("Statue of [L]iberty or [S]atellite\n")
+
+    if hack in 'Ll':
+        liberty()
+    if hack in 'Cc':
+        satellite()
+    else:
+        print('Invalid Entry. Try again.')
+        start()
+
+def progress():
+    toolbar_width = 40
+
+    sys.stdout.write("[%s]" % (" " * toolbar_width))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (toolbar_width + 1))  # return to start of line, after '['
+
+    for i in range(toolbar_width):
+        time.sleep(random.uniform(.01,.25))  # do real work here
+        # update the bar
+        sys.stdout.write("-")
+        sys.stdout.flush()
+
+    sys.stdout.write("\n")
+
+    #for i in range(1, 100 + 1):
+     #   print(i, '%')
+     #   time.sleep(.25)
+def liberty():
+    print("INFlILTRATING MAINFRAME\n")
+    progress()
+    end()
+
+
+def satellite():
+    print("INFILTRATING NASA")
+    progress()
+    print("done")
+    time.sleep(1)
+    print("Do you want to broadcast a message?\n")
+    broadcast = input("[Y]es or [N]o")
+    if broadcast in "Yy":
+        message()
+    if broadcast in "Nn":
+        end()
+    else:
+        print("Invalid entry. Try again.")
+        satellite()
+
+def message():
+    message = input("Enter broadcast message:")
+    progress()
+    time.sleep(.2)
+    print("Satellite is now broadcasting\"", message,"\"across space.\n")
+    end()
+
+def end():
+    print("Hacking complete.\n")
+    print("Hack again?")
+    restart = input("[Y]es or [N]o\n")
+    if restart in "Yy":
+        start()
+    if restart in "Nn":
+        import sys
+        sys.exit("TERMINATING")
+
+start()
